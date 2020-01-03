92a0f55d95c095a8b819b76e416a34128cedc9b3
Dragos Moldovan-Grunfeld <dgrunfeld@mango-solutions.com>
2020-01-03 17:12:18 +0000

    add add_three

--- R/add_two.R
+++ R/add_two.R
@@ -10,3 +10,16 @@
 add_two <- function(x) {
   x + 2
 }
+
+#' Add three
+#'
+#' @param x a numeric vector
+#'
+#' @return a numeric vector
+#' @export
+#'
+#' @examples
+#' add_three(3)
+add_three <- function(x) {
+    x + 2
+}
